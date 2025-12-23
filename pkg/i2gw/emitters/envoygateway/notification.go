1f016ed5aeb5959e05e86b22a9aa7afa33084591
kkk777-7 <kota.kimura0725@gmail.com>
2025-12-24 01:13:24 +0900

    fix golang lint
    Signed-off-by: kkk777-7 <kota.kimura0725@gmail.com>

--- pkg/i2gw/emitters/envoygateway/notification.go
+++ pkg/i2gw/emitters/envoygateway/notification.go
@@ -1,8 +1,9 @@
 package envoygateway_emitter
 
 import (
-	"github.com/kkk777-7/ingress2eg/pkg/i2gw/notifications"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	"github.com/kkk777-7/ingress2eg/pkg/i2gw/notifications"
 )
 
 func notify(mType notifications.MessageType, message string, callingObject ...client.Object) {
