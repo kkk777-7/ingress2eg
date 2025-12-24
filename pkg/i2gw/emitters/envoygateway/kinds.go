9a88e8ea751be31b1225fb9404c7ff808e4f410b
kkk777-7 <kota.kimura0725@gmail.com>
2025-12-24 15:28:19 +0900

    fix golang lint
    Signed-off-by: kkk777-7 <kota.kimura0725@gmail.com>

--- pkg/i2gw/emitters/envoygateway/kinds.go
+++ pkg/i2gw/emitters/envoygateway/kinds.go
@@ -2,10 +2,8 @@ package envoygateway_emitter
 
 import "k8s.io/apimachinery/pkg/runtime/schema"
 
-var (
-	HTTPRouteFilterGVK = schema.GroupVersionKind{
-		Group:   "gateway.envoyproxy.io",
-		Version: "v1alpha1",
-		Kind:    "HTTPRouteFilter",
-	}
-)
+var HTTPRouteFilterGVK = schema.GroupVersionKind{
+	Group:   "gateway.envoyproxy.io",
+	Version: "v1alpha1",
+	Kind:    "HTTPRouteFilter",
+}
