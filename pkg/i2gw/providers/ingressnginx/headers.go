a690630c862b6d987464d63b58cb36d8359010c7
kkk777-7 <kota.kimura0725@gmail.com>
2025-12-26 10:24:03 +0900

    feat: support forwarded prefix annotations
    Signed-off-by: kkk777-7 <kota.kimura0725@gmail.com>

---
+++ pkg/i2gw/providers/ingressnginx/headers.go
@@ -0,0 +1,81 @@
+package ingressnginx
+
+import (
+	"fmt"
+
+	networkingv1 "k8s.io/api/networking/v1"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/apimachinery/pkg/util/validation/field"
+	gwapiv1 "sigs.k8s.io/gateway-api/apis/v1"
+
+	emitterir "github.com/kkk777-7/ingress2eg/pkg/i2gw/emitter_intermediate"
+	"github.com/kkk777-7/ingress2eg/pkg/i2gw/notifications"
+	providerir "github.com/kkk777-7/ingress2eg/pkg/i2gw/provider_intermediate"
+	"github.com/kkk777-7/ingress2eg/pkg/i2gw/providers/common"
+)
+
+const (
+	forwardedPrefixAnnotation = "nginx.ingress.kubernetes.io/x-forwarded-prefix"
+)
+
+func headerFeature(ingresses []networkingv1.Ingress, _ map[types.NamespacedName]map[string]int32, pir *providerir.ProviderIR, eir *emitterir.EmitterIR) field.ErrorList {
+	ruleGroups := common.GetRuleGroups(ingresses)
+	var errList field.ErrorList
+
+	for _, rg := range ruleGroups {
+		key := types.NamespacedName{Namespace: rg.Namespace, Name: common.RouteName(rg.Name, rg.Host)}
+
+		// Get RuleBackendSources from Provider IR
+		providerHTTPRouteContext, ok := pir.HTTPRoutes[key]
+		if !ok {
+			continue
+		}
+
+		emitterHTTPRouteContext, ok := eir.HTTPRoutes[key]
+		if !ok {
+			continue
+		}
+
+		for ruleIdx, backendSources := range providerHTTPRouteContext.RuleBackendSources {
+			if ruleIdx >= len(emitterHTTPRouteContext.Spec.Rules) {
+				errList = append(errList, field.InternalError(
+					field.NewPath("httproute", emitterHTTPRouteContext.HTTPRoute.Name, "spec", "rules").Index(ruleIdx),
+					fmt.Errorf("rule index %d exceeds available rules", ruleIdx),
+				))
+				continue
+			}
+
+			for _, source := range backendSources {
+				if source.Ingress == nil {
+					continue
+				}
+				ingress := *source.Ingress
+
+				if val := ingress.Annotations[forwardedPrefixAnnotation]; val != "" {
+					emitterHTTPRouteContext.Spec.Rules[ruleIdx].Filters = append(emitterHTTPRouteContext.Spec.Rules[ruleIdx].Filters,
+						gwapiv1.HTTPRouteFilter{
+							Type: gwapiv1.HTTPRouteFilterRequestHeaderModifier,
+							RequestHeaderModifier: &gwapiv1.HTTPHeaderFilter{
+								Add: []gwapiv1.HTTPHeader{
+									{
+										Name:  "X-Forwarded-Prefix",
+										Value: val,
+									},
+								},
+							},
+						},
+					)
+
+					notify(notifications.InfoNotification, fmt.Sprintf("parsed Header (x-forwarded-prefix) annotations of ingress %s/%s", ingress.Namespace, ingress.Name),
+						&emitterHTTPRouteContext.HTTPRoute)
+				}
+			}
+		}
+		eir.HTTPRoutes[key] = emitterHTTPRouteContext
+	}
+
+	if len(errList) > 0 {
+		return errList
+	}
+	return nil
+}
