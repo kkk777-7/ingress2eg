a1886c86b782dfe7022eba7b4919a646e84feee4
kkk777-7 <kota.kimura0725@gmail.com>
2026-01-01 18:12:46 +0900

    add e2e controller/apps setup
    Signed-off-by: kkk777-7 <kota.kimura0725@gmail.com>

---
+++ test/e2e/tools/eg-deploy.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+# Envoy Gateway version
+EG_VERSION="v1.6.1"
+
+# Get the directory where this script is located
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+EG_VALUES_FILE="${SCRIPT_DIR}/eg-values.yaml"
+
+helm template eg oci://docker.io/envoyproxy/gateway-crds-helm \
+  --version "${EG_VERSION}" \
+  --set crds.gatewayAPI.enabled=true \
+  --set crds.gatewayAPI.channel=standard \
+  --set crds.envoyGateway.enabled=true \
+  | kubectl apply --server-side -f -
+
+helm install eg oci://docker.io/envoyproxy/gateway-helm \
+  --version "${EG_VERSION}" \
+  -n envoy-gateway-system \
+  --create-namespace \
+  -f "${EG_VALUES_FILE}" \
+  --skip-crds
